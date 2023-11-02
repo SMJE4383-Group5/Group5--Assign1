5551e1733f158569968a500630ff02371eb7c296
SalmaYasserSadik <sadik.salma@graduate.utm.my>
2023-11-02 12:46:32 +0800

    commit

--- # Generate a hailstone sequence.py
+++ # Generate a hailstone sequence.py
@@ -1,17 +1,19 @@
-# Generate a hailstone sequence
-number_str = input ("Enter a positive integer:") 
-number = int(number_str) 
+# Generate a hailstone sequence 
+number_str = input("Enter a positive integer: ")
+number = int(number_str)
 count = 0
-print ("Starting with number:", number) 
-print ("Sequence is: ", end=' ')
 
-while number > 1: # stop when the sequence reaches 1
-   if number % 2:  #number is odd
-    number = number*3 + 1
-   else: #number is even 
-    number = number/2 
-   print (number, ",", end='') #add number to sequence
-   count +=1 #add to the count
-# else:
-# print() #blank line for nicer output 
-# print ("Sequence is ", count," numbers long")
+print("Starting with number:", number)
+print("Sequence is:", end=' ')
+
+while number > 1:  # Stop when the sequence reaches 1
+    if number % 2:  # Number is odd
+        number = number * 3 + 1
+    else:  # Number is even
+        number = number // 2  # Use integer division to ensure the result is an integer
+    print(number, end=', ')  # Add number to the sequence
+    count += 1  # Add to the count
+
+# After the loop, print the count and a newline for nicer output
+print()
+print("Sequence is", count, "numbers long")
---
+++ give a number.py
@@ -0,0 +1,11 @@
+a = int(input("Give a number:"))
+b, c = 1, 0
+
+while b <= a:
+    c = c + b
+    b = b + 1
+
+print("a:", a)
+print("b:", b)
+print("c:", c)
+print("Result:", float(c) / b - 1)
